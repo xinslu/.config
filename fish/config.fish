8e4e170073f6b79cd6f81b7590c0d3c2759dc674
Kinshuk Phalke <phalkekinshuk@gmail.com>
2022-07-29 09:48:18 -0400

    Added some stuff with better keybind

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,9 @@
 if status is-interactive
-    set -gx PATH /Library/Frameworks/Python.framework/Versions/3.9/bin $HOME/.cargo/bin $PATH
     set -Ux EDITOR nvim
     set -Ux VISUAL nvim
     set --erase fish_greeting
     alias python="python3"
+    set node_options --openssl-legacy-provider 
 end
 starship init fish | source
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
 
