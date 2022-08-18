9cf3149dee88dca56bd7d7da205b04d6cceb32a9
Kinshuk Phalke <phalkekinshuk@gmail.com>
2022-08-18 09:33:21 +0400

    Added some stuff

--- nvim/lua/configs.lua
+++ nvim/lua/configs.lua
@@ -1,31 +1,19 @@
 local set = vim.opt -- global options
 local cmd = vim.cmd -- execute Vim commands
 vim.g.mapleader = " "
-require('configs.terminal')
-require('nvim_comment').setup()
-require('configs.trouble')
-
--- Colors --
 vim.g.gruvbox_improved_warnings = 1
 set.termguicolors = true
 vim.g.gruvbox_contrast_hard = 'hard'
+require('configs.terminal')
+require('nvim_comment').setup()
+require('configs.trouble')
+require('keymaps')
 require("highlights")
-----
 
 -- Formatting --
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
 vim.cmd [[autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll]]
 vim.cmd [[autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js Prettier]]
-----
-
--- Keymaps --
-vim.api.nvim_set_keymap('n', '<CR>', ':noh<CR><CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-W>j', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-W>k', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-W>l', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-W>h', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-b>', ':lua require"configs.build".openTerm()<CR>', { noremap = true })
 ----
 
 -- Autocmds --
@@ -34,8 +22,6 @@ vim.api.nvim_create_autocmd("TermOpen",
     { command = "setlocal nobuflisted nonumber norelativenumber", group = group })
 vim.api.nvim_create_autocmd("InsertEnter", { command = ":let @/=\"\"" })
 ----
-
-
 
 set.guicursor = ""
 set.tabstop = 4
